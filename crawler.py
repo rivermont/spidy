144b8c69b5e99123d646296cb03d4fa948bce2ee
rivermont <william.11bennett@gmail.com>
2017-05-20 21:39:27 -0400

    Updated crawler.py

--- crawler.py
+++ crawler.py
@@ -181,10 +181,10 @@ if len(todo) == 0:
 	todo += start
 
 after = before - len(todo)
+removedCount += after
 
 print('[INIT]: {0} invalid links removed from TODO.'.format(after))
 
-removedCount += after
 
 print('[INIT]: TODO first value: {0}'.format(todo[0]))
 
