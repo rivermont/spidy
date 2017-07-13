03a14104639ecb2fed22712d3fe8b1a33b682e65
Will Bennett <william.11bennett@gmail.com>
2017-07-13 16:59:20 -0400

    Update gui.py

--- gui.py
+++ gui.py
@@ -58,4 +58,12 @@ controlBox.rowconfigure(0, weight=1)
 
 statusBox = ttk.Frame(rightBar, padding='2')
 statusBox.grid(column=0, row=1, sticky=(E, W))
+statusBox.columnconfigure(0, weight=1)
+statusBox.rowconfigure(1, weight=1)
+
+consoleBox = ttk.Frame(rightBar, padding='2')
+consoleBox.grid(column=0, row=2)
+consoleBox.columnconfigure(0, weight=1)
+consoleBox.rowcomfigure(2, weight=1)
+
 
