e528b0573f484706ff44568be542d043cff8f2c9
rivermont <william.11bennett@gmail.com>
2017-07-17 16:17:57 -0400

    Updated gui.py

--- gui.py
+++ gui.py
@@ -32,7 +32,7 @@ window.title('spidy Web Crawler - by rivermont')
 window.iconbitmap('{0}\\media\\favicon.ico'.format(DIR))
 
 #Frame to fill main window
-mainFrame = ttk.Frame(window, padding='5')
+mainFrame = ttk.Frame(window, padding='4')
 mainFrame.grid(column=0, row=0, sticky=(N, W, E, S))
 mainFrame.columnconfigure(0, weight=1)
 mainFrame.rowconfigure(0, weight=1)
@@ -55,82 +55,82 @@ MaxNewMIMEs = IntVar()
 CustomHeaders = StringVar()
 
 #Container to hold variable settings
-settingBox = ttk.Frame(mainFrame, padding='5', borderwidth=1, relief='solid')
+settingBox = ttk.Frame(mainFrame, padding='4', borderwidth=1, relief='solid')
 settingBox.grid(column=0, row=0, sticky=(N, S, W))
 settingBox.columnconfigure(0, weight=1)
 settingBox.rowconfigure(0, weight=1)
 
 #Container for things on the right side of the main window
-rightBar = ttk.Frame(mainFrame, padding='5', borderwidth=1, relief='solid')
+rightBar = ttk.Frame(mainFrame, padding='4', borderwidth=1, relief='solid')
 rightBar.grid(column=1, row=0, sticky=(N, S, E))
 rightBar.columnconfigure(2, weight=1)
 rightBar.rowconfigure(0, weight=1)
 
 #Container for controlling the crawler
-controlBox = ttk.Frame(rightBar, padding='5', borderwidth=1, relief='solid')
+controlBox = ttk.Frame(rightBar, padding='4', borderwidth=1, relief='solid')
 controlBox.grid(column=1, row=0, sticky=(N, E, W))
 controlBox.columnconfigure(1, weight=1)
 controlBox.rowconfigure(0, weight=1)
 
 #Container for the status elements
-statusBox = ttk.Frame(rightBar, padding='5', borderwidth=1, relief='solid')
+statusBox = ttk.Frame(rightBar, padding='4', borderwidth=1, relief='solid')
 statusBox.grid(column=0, row=1, sticky=(E, W))
 statusBox.columnconfigure(0, weight=1)
 statusBox.rowconfigure(1, weight=1)
 
 #Container for the console log
-consoleBox = ttk.Frame(rightBar, padding='5', borderwidth=1, relief='solid')
+consoleBox = ttk.Frame(rightBar, padding='4', borderwidth=1, relief='solid')
 consoleBox.grid(column=0, row=2)
 consoleBox.columnconfigure(0, weight=1)
 consoleBox.rowconfigure(2, weight=1)
 
 #Button to pause the crawler
-pauseButton = ttk.Button(controlBox, padding='5', text='Pause')
+pauseButton = ttk.Button(controlBox, padding='4', text='Pause')
 pauseButton.grid(column=0, row=0, sticky=(N, S, W))
 pauseButton.columnconfigure(0, weight=1)
 pauseButton.rowconfigure(0, weight=1)
 
 #Button to start the crawler
-goButton = ttk.Button(controlBox, padding='5', text='Go')
+goButton = ttk.Button(controlBox, padding='4', text='Go')
 goButton.grid(column=1, row=0, sticky=(N, S))
 goButton.columnconfigure(1, weight=1)
 goButton.rowconfigure(0, weight=1)
 
 #Button to stop the crawler
-stopButton = ttk.Button(controlBox, padding='5', text='Stop')
+stopButton = ttk.Button(controlBox, padding='4', text='Stop')
 stopButton.grid(column=2, row=0, sticky=(N, S, E))
 stopButton.columnconfigure(2, weight=1)
 stopButton.rowconfigure(0, weight=1)
 
 #Title for crawler setting area
-ttk.Label(settingBox, text='Crawler Settings', padding='5').grid(column=0, row=0, columnspan=4, sticky=(N ,S))
+ttk.Label(settingBox, text='Crawler Settings').grid(column=0, row=0, columnspan=4, sticky=(N ,S))
 
 #Option to set Overwrite
-overwriteCheck = ttk.Checkbutton(settingBox, text='Overwrite', variable=Overwrite, padding='5')
+overwriteCheck = ttk.Checkbutton(settingBox, text='Overwrite', variable=Overwrite)
 overwriteCheck.grid(column=0, row=1, columnspan=2, sticky=(W))
 overwriteCheck.columnconfigure(0, weight=1)
 overwriteCheck.rowconfigure(1, weight=1)
 
 #Option to set RaiseErrors
-raiseErrorsCheck = ttk.Checkbutton(settingBox, text='Raise Errors', variable=RaiseErrors, padding='5')
+raiseErrorsCheck = ttk.Checkbutton(settingBox, text='Raise Errors', variable=RaiseErrors)
 raiseErrorsCheck.grid(column=0, row=2, columnspan=2, sticky=(W))
 raiseErrorsCheck.columnconfigure(0, weight=1)
 raiseErrorsCheck.rowconfigure(2, weight=1)
 
 #Option to set SavePages
-savePagesCheck = ttk.Checkbutton(settingBox, text='Save Pages', variable=SavePages, padding='5')
+savePagesCheck = ttk.Checkbutton(settingBox, text='Save Pages', variable=SavePages)
 savePagesCheck.grid(column=0, row=3, columnspan=2, sticky=(W))
 savePagesCheck.columnconfigure(0, weight=1)
 savePagesCheck.rowconfigure(3, weight=1)
 
 #Option to set ZipFiles
-zipFilesCheck = ttk.Checkbutton(settingBox, text='Zip Files', variable=ZipFiles, padding='5')
+zipFilesCheck = ttk.Checkbutton(settingBox, text='Zip Files', variable=ZipFiles)
 zipFilesCheck.grid(column=0, row=4, columnspan=2, sticky=(W))
 zipFilesCheck.columnconfigure(0, weight=1)
 zipFilesCheck.rowconfigure(4, weight=1)
 
 #Option to set SaveWords
-saveWordsCheck = ttk.Checkbutton(settingBox, text='Save Words', variable=SaveWords, padding='5')
+saveWordsCheck = ttk.Checkbutton(settingBox, text='Save Words', variable=SaveWords)
 saveWordsCheck.grid(column=0, row=5, columnspan=2, sticky=(W))
 saveWordsCheck.columnconfigure(0, weight=1)
 saveWordsCheck.rowconfigure(5, weight=1)
