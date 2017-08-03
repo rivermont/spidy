9301e782e0588ad444ac230fcf8d6e1466ae3d90
rivermont <william.11bennett@gmail.com>
2017-08-02 22:15:48 -0400

    PEP 8 Compliance

--- gui.py
+++ gui.py
@@ -111,6 +111,7 @@ def check_link(item):
 				return True
 	return False
 
+
 def check_word(word):
 	"""
 	Returns True if word is not valid.
@@ -122,6 +123,7 @@ def check_word(word):
 	else:
 		return False
 
+
 def check_path(file_path):
 	"""
 	Checks the path of a given filename to see whether it will cause errors when saving.
@@ -132,6 +134,7 @@ def check_path(file_path):
 		return False
 	else:
 		return True
+
 
 def make_words(site):
 	"""
@@ -145,6 +148,7 @@ def make_words(site):
 		if check_word(word):  # If word is invalid
 			word_list.remove(word)  # Remove invalid word from list
 	return word_list
+
 
 def save_files(word_list):
 	"""
@@ -172,6 +176,7 @@ def save_files(word_list):
 
 	update_file(BAD_FILE, BAD_LINKS, 'bad links')
 
+
 def make_file_path(url, ext):
 	"""
 	Makes a valid Windows file path for a url.
@@ -184,6 +189,7 @@ def make_file_path(url, ext):
 	url = url[:255]  # Truncate to valid file length
 	return url
 
+
 def get_mime_type(page):
 	"""
 	Extracts the Content-Type header from the headers returned by page.
@@ -193,6 +199,7 @@ def get_mime_type(page):
 		return doc_type
 	except KeyError:  # If no Content-Type was returned, return blank
 		return ''
+
 
 def mime_lookup(value):
 	"""
@@ -210,6 +217,7 @@ def mime_lookup(value):
 		Beep(1000, 100)
 		raise HeaderError('Unknown MIME type: {0}'.format(value))
 
+
 def save_page(url, page):
 	"""
 	Download content of url and save to the save folder.
@@ -223,6 +231,7 @@ def save_page(url, page):
 	with open(file_path, 'wb+') as file:
 		file.write(page.content)
 
+
 def update_file(file, content, file_type):
 	with open(file, 'r+') as open_file:  # Open save file for reading and writing
 		file_content = open_file.readlines()  # Make list of all lines in file
@@ -234,6 +243,7 @@ def update_file(file, content, file_type):
 			open_file.write('\n' + str(item))  # Write all words to file
 		open_file.truncate()  # Delete everything in file beyond what has been written (old stuff)
 	write_log('[LOG]: Saved {0} {1} to {2}'.format(len(content), file_type, file))
+
 
 def info_log():
 	"""
@@ -250,6 +260,7 @@ def info_log():
 	write_log('[INFO]: {0}/{1} new MIMEs found.'.format(NEW_MIME_COUNT, MAX_NEW_MIMES))
 	write_log('[INFO]: {0}/{1} known errors caught.'.format(KNOWN_ERROR_COUNT, MAX_KNOWN_ERRORS))
 
+
 def log(message):
 	"""
 	Logs a single message to the error log file.
@@ -261,6 +272,7 @@ def log(message):
 		file.write(message)  # Write message
 		file.write(LOG_END)  # Write closing line
 
+
 def handle_keyboard_interrupt():
 	write_log('[ERR]: User performed a KeyboardInterrupt, stopping crawler...')
 	log('\nLOG: User performed a KeyboardInterrupt, stopping crawler.')
@@ -268,12 +280,14 @@ def handle_keyboard_interrupt():
 	LOG_FILE.close()
 	exit()
 
+
 def handle_invalid_input(type_='input'):
 	"""
 	Handles an invalid user input, usually from the input() function.
 	"""
 	LOG_FILE.write('\n[{0}] [spidy] [ERR]: Please enter a valid {1}. (yes/no)'.format(get_time(), type_))
 	raise SyntaxError('[{0}] [spidy] [ERR]: Please enter a valid {1}. (yes/no)'.format(get_time(), type_))
+
 
 def err_log(url, error1, error2):
 	"""
@@ -286,6 +300,7 @@ def err_log(url, error1, error2):
 		work_log.write('\n\n=====ERROR=====')  # Write opening line
 		work_log.write('\nTIME: {0}\nURL: {1}\nERROR: {2}\nEXT: {3}'.format(time, url, error1, str(error2)))
 		work_log.write(LOG_END)  # Write closing line
+
 
 def zip_files(out_file_name, directory):
 	"""
@@ -399,7 +414,7 @@ HEADERS = {
 	},
 	'Chrome': {
 		'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'  # Line broken up to appease PEP 8
-					  'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36',
+		'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36',
 		'Accept-Language': 'en_US, en-US, en',
 		'Accept-Encoding': 'gzip',
 		'Connection': 'keep-alive'
@@ -418,7 +433,7 @@ HEADERS = {
 	},
 	'Edge': {
 		'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'  # Line broken up to appease PEP 8
-					  'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36 Edge/15.15063',
+		'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36 Edge/15.15063',
 		'Accept-Language': 'en_US, en-US, en',
 		'Accept-Encoding': 'gzip',
 		'Connection': 'keep-alive'
@@ -468,196 +483,196 @@ def setup_window():
 	window.title('spidy Web Crawler - by rivermont')
 	window.iconbitmap('{0}\\media\\favicon.ico'.format(CRAWLER_DIR))
 
-	Overwrite = BooleanVar()
-	RaiseErrors = BooleanVar()
-	SavePages = BooleanVar()
-	ZipFiles = BooleanVar()
-	SaveWords = BooleanVar()
-	TodoFile = StringVar()
-	DoneFile = StringVar()
-	BadFile = StringVar()
-	WordFile = StringVar()
-	SaveCount = IntVar()
-	MaxNewErrors = IntVar()
-	MaxHTTPErrors = IntVar()
-	MaxKnownErrors = IntVar()
-	MaxNewMIMEs = IntVar()
-	CustomHeaders = StringVar()
+	overwrite = BooleanVar()
+	raise_errors = BooleanVar()
+	save_pages = BooleanVar()
+	zip_files = BooleanVar()
+	save_words = BooleanVar()
+	todo_file = StringVar()
+	done_file = StringVar()
+	bad_file = StringVar()
+	word_file = StringVar()
+	save_count = IntVar()
+	max_new_errors = IntVar()
+	max_http_errors = IntVar()
+	max_known_errors = IntVar()
+	max_new_mimes = IntVar()
+	custom_headers = StringVar()
 
 	# Frame to fill main window
-	mainFrame = ttk.Frame(window, padding='4')
-	mainFrame.grid(column=0, row=0, sticky=(N, W, E, S))
-	mainFrame.columnconfigure(0, weight=1)
-	mainFrame.rowconfigure(0, weight=1)
+	main_frame = ttk.Frame(window, padding='4')
+	main_frame.grid(column=0, row=0, sticky=(N, W, E, S))
+	main_frame.columnconfigure(0, weight=1)
+	main_frame.rowconfigure(0, weight=1)
 
 	# Container to hold variable settings
-	settingBox = ttk.Frame(mainFrame, padding='4', borderwidth=1, relief='solid')
-	settingBox.grid(column=0, row=0, sticky=(N, S, W))
-	settingBox.columnconfigure(0, weight=1)
-	settingBox.rowconfigure(0, weight=1)
+	setting_box = ttk.Frame(main_frame, padding='4', borderwidth=1, relief='solid')
+	setting_box.grid(column=0, row=0, sticky=(N, S, W))
+	setting_box.columnconfigure(0, weight=1)
+	setting_box.rowconfigure(0, weight=1)
 
 	# Container for things on the right side of the main window
-	rightBar = ttk.Frame(mainFrame, padding='4', borderwidth=1, relief='solid')
-	rightBar.grid(column=1, row=0, sticky=(N, S, E))
-	rightBar.columnconfigure(2, weight=1)
-	rightBar.rowconfigure(0, weight=1)
+	right_bar = ttk.Frame(main_frame, padding='4', borderwidth=1, relief='solid')
+	right_bar.grid(column=1, row=0, sticky=(N, S, E))
+	right_bar.columnconfigure(2, weight=1)
+	right_bar.rowconfigure(0, weight=1)
 
 	# Container for controlling the crawler
-	controlBox = ttk.Frame(rightBar, padding='4', borderwidth=1, relief='solid')
-	controlBox.grid(column=1, row=0, sticky=(N, E, W))
-	controlBox.columnconfigure(1, weight=1)
-	controlBox.rowconfigure(0, weight=1)
+	control_box = ttk.Frame(right_bar, padding='4', borderwidth=1, relief='solid')
+	control_box.grid(column=1, row=0, sticky=(N, E, W))
+	control_box.columnconfigure(1, weight=1)
+	control_box.rowconfigure(0, weight=1)
 
 	# Container for the status elements
-	statusBox = ttk.Frame(rightBar, padding='4', borderwidth=1, relief='solid')
-	statusBox.grid(column=0, row=1, sticky=(E, W))
-	statusBox.columnconfigure(0, weight=1)
-	statusBox.rowconfigure(1, weight=1)
+	status_box = ttk.Frame(right_bar, padding='4', borderwidth=1, relief='solid')
+	status_box.grid(column=0, row=1, sticky=(E, W))
+	status_box.columnconfigure(0, weight=1)
+	status_box.rowconfigure(1, weight=1)
 
 	# Container for the console log
-	consoleBox = ttk.Frame(rightBar, padding='4', borderwidth=1, relief='solid')
-	consoleBox.grid(column=0, row=2)
-	consoleBox.columnconfigure(0, weight=1)
-	consoleBox.rowconfigure(2, weight=1)
+	console_box = ttk.Frame(right_bar, padding='4', borderwidth=1, relief='solid')
+	console_box.grid(column=0, row=2)
+	console_box.columnconfigure(0, weight=1)
+	console_box.rowconfigure(2, weight=1)
 
 	# Button to pause the crawler
-	pauseButton = ttk.Button(controlBox, padding='4', text='Pause')
-	pauseButton.grid(column=0, row=0, sticky=(N, S, W))
-	pauseButton.columnconfigure(0, weight=1)
-	pauseButton.rowconfigure(0, weight=1)
+	pause_button = ttk.Button(control_box, padding='4', text='Pause')
+	pause_button.grid(column=0, row=0, sticky=(N, S, W))
+	pause_button.columnconfigure(0, weight=1)
+	pause_button.rowconfigure(0, weight=1)
 
 	# Button to start the crawler
-	goButton = ttk.Button(controlBox, command=start_crawler, padding='4', text='Go')
-	goButton.grid(column=1, row=0, sticky=(N, S))
-	goButton.columnconfigure(1, weight=1)
-	goButton.rowconfigure(0, weight=1)
+	go_button = ttk.Button(control_box, command=start_crawler, padding='4', text='Go')
+	go_button.grid(column=1, row=0, sticky=(N, S))
+	go_button.columnconfigure(1, weight=1)
+	go_button.rowconfigure(0, weight=1)
 
 	# Button to stop the crawler
-	stopButton = ttk.Button(controlBox, padding='4', text='Stop')
-	stopButton.grid(column=2, row=0, sticky=(N, S, E))
-	stopButton.columnconfigure(2, weight=1)
-	stopButton.rowconfigure(0, weight=1)
+	stop_button = ttk.Button(control_box, padding='4', text='Stop')
+	stop_button.grid(column=2, row=0, sticky=(N, S, E))
+	stop_button.columnconfigure(2, weight=1)
+	stop_button.rowconfigure(0, weight=1)
 
 	# Title for crawler setting area
-	ttk.Label(settingBox, text='Crawler Settings').grid(column=0, row=0, columnspan=4, sticky=(N, S))
+	ttk.Label(setting_box, text='Crawler Settings').grid(column=0, row=0, columnspan=4, sticky=(N, S))
 
 	# Option to set Overwrite
-	overwriteCheck = ttk.Checkbutton(settingBox, text='Overwrite', variable=Overwrite)
-	overwriteCheck.grid(column=0, row=1, columnspan=2, sticky=W)
-	overwriteCheck.columnconfigure(0, weight=1)
-	overwriteCheck.rowconfigure(1, weight=1)
+	overwrite_check = ttk.Checkbutton(setting_box, text='Overwrite', variable=overwrite)
+	overwrite_check.grid(column=0, row=1, columnspan=2, sticky=W)
+	overwrite_check.columnconfigure(0, weight=1)
+	overwrite_check.rowconfigure(1, weight=1)
 
 	# Option to set RaiseErrors
-	raiseErrorsCheck = ttk.Checkbutton(settingBox, text='Raise Errors', variable=RaiseErrors)
-	raiseErrorsCheck.grid(column=0, row=2, columnspan=2, sticky=W)
-	raiseErrorsCheck.columnconfigure(0, weight=1)
-	raiseErrorsCheck.rowconfigure(2, weight=1)
+	raise_errors_check = ttk.Checkbutton(setting_box, text='Raise Errors', variable=raise_errors)
+	raise_errors_check.grid(column=0, row=2, columnspan=2, sticky=W)
+	raise_errors_check.columnconfigure(0, weight=1)
+	raise_errors_check.rowconfigure(2, weight=1)
 
 	# Option to set SavePages
-	savePagesCheck = ttk.Checkbutton(settingBox, text='Save Pages', variable=SavePages)
-	savePagesCheck.grid(column=0, row=3, columnspan=2, sticky=W)
-	savePagesCheck.columnconfigure(0, weight=1)
-	savePagesCheck.rowconfigure(3, weight=1)
+	save_pages_check = ttk.Checkbutton(setting_box, text='Save Pages', variable=save_pages)
+	save_pages_check.grid(column=0, row=3, columnspan=2, sticky=W)
+	save_pages_check.columnconfigure(0, weight=1)
+	save_pages_check.rowconfigure(3, weight=1)
 
 	# Option to set ZipFiles
-	zipFilesCheck = ttk.Checkbutton(settingBox, text='Zip Files', variable=ZipFiles)
-	zipFilesCheck.grid(column=0, row=4, columnspan=2, sticky=W)
-	zipFilesCheck.columnconfigure(0, weight=1)
-	zipFilesCheck.rowconfigure(4, weight=1)
+	zip_files_check = ttk.Checkbutton(setting_box, text='Zip Files', variable=zip_files)
+	zip_files_check.grid(column=0, row=4, columnspan=2, sticky=W)
+	zip_files_check.columnconfigure(0, weight=1)
+	zip_files_check.rowconfigure(4, weight=1)
 
 	# Option to set SaveWords
-	saveWordsCheck = ttk.Checkbutton(settingBox, text='Save Words', variable=SaveWords)
-	saveWordsCheck.grid(column=0, row=5, columnspan=2, sticky=W)
-	saveWordsCheck.columnconfigure(0, weight=1)
-	saveWordsCheck.rowconfigure(5, weight=1)
+	save_words_check = ttk.Checkbutton(setting_box, text='Save Words', variable=save_words)
+	save_words_check.grid(column=0, row=5, columnspan=2, sticky=W)
+	save_words_check.columnconfigure(0, weight=1)
+	save_words_check.rowconfigure(5, weight=1)
 
 	# Field to enter number for SaveCount
-	ttk.Label(settingBox, text='Save Count').grid(column=0, row=6, columnspan=2, sticky=W)
+	ttk.Label(setting_box, text='Save Count').grid(column=0, row=6, columnspan=2, sticky=W)
 
-	saveCountEntry = ttk.Entry(settingBox, width=5, textvariable=SaveCount)
-	saveCountEntry.grid(column=0, row=7, sticky=(E, W))
-	saveCountEntry.columnconfigure(0, weight=1)
-	saveCountEntry.rowconfigure(7, weight=1)
+	save_count_entry = ttk.Entry(setting_box, width=5, textvariable=save_count)
+	save_count_entry.grid(column=0, row=7, sticky=(E, W))
+	save_count_entry.columnconfigure(0, weight=1)
+	save_count_entry.rowconfigure(7, weight=1)
 
 	# Field to enter custom headers
-	ttk.Label(settingBox, text='Custom Headers').grid(column=0, row=8, columnspan=2, sticky=W)
+	ttk.Label(setting_box, text='Custom Headers').grid(column=0, row=8, columnspan=2, sticky=W)
 
-	customHeadersEntry = Text(settingBox, height=3, width=16)
-	customHeadersEntry.grid(column=0, row=9, columnspan=2, sticky=W)
-	customHeadersEntry.columnconfigure(0, weight=1)
-	customHeadersEntry.rowconfigure(9, weight=1)
+	custom_headers_entry = Text(setting_box, height=3, width=16)
+	custom_headers_entry.grid(column=0, row=9, columnspan=2, sticky=W)
+	custom_headers_entry.columnconfigure(0, weight=1)
+	custom_headers_entry.rowconfigure(9, weight=1)
 
 	# Field to enter custom starting links
-	ttk.Label(settingBox, text='Start Links').grid(column=0, row=10, columnspan=2, sticky=W)
+	ttk.Label(setting_box, text='Start Links').grid(column=0, row=10, columnspan=2, sticky=W)
 
-	customStartLinks = Text(settingBox, height=2, width=16)
-	customStartLinks.grid(column=0, row=11, columnspan=2, sticky=W)
-	customStartLinks.columnconfigure(0, weight=1)
-	customStartLinks.rowconfigure(11, weight=1)
+	custom_start_links = Text(setting_box, height=2, width=16)
+	custom_start_links.grid(column=0, row=11, columnspan=2, sticky=W)
+	custom_start_links.columnconfigure(0, weight=1)
+	custom_start_links.rowconfigure(11, weight=1)
 
 	# Button to select todo file
-	getTodoFileButton = ttk.Button(settingBox, text='...', command=get_file)
-	getTodoFileButton.grid(column=2, row=1, sticky=W)
-	getTodoFileButton.columnconfigure(1, weight=1)
-	getTodoFileButton.rowconfigure(2, weight=1)
+	get_todo_file_button = ttk.Button(setting_box, text='...', command=get_file)
+	get_todo_file_button.grid(column=2, row=1, sticky=W)
+	get_todo_file_button.columnconfigure(1, weight=1)
+	get_todo_file_button.rowconfigure(2, weight=1)
 
-	ttk.Label(settingBox, text='TODO File').grid(column=3, row=1, sticky=W)
+	ttk.Label(setting_box, text='TODO File').grid(column=3, row=1, sticky=W)
 
 	# Button to select done file
-	getDoneFileButton = ttk.Button(settingBox, text='...', command=get_file)
-	getDoneFileButton.grid(column=2, row=2, sticky=W)
-	getDoneFileButton.columnconfigure(2, weight=1)
-	getDoneFileButton.rowconfigure(2, weight=1)
+	get_done_file_button = ttk.Button(setting_box, text='...', command=get_file)
+	get_done_file_button.grid(column=2, row=2, sticky=W)
+	get_done_file_button.columnconfigure(2, weight=1)
+	get_done_file_button.rowconfigure(2, weight=1)
 
-	ttk.Label(settingBox, text='Done File').grid(column=3, row=2, sticky=W)
+	ttk.Label(setting_box, text='Done File').grid(column=3, row=2, sticky=W)
 
 	# Button to select bad link file
-	getBadFileButton = ttk.Button(settingBox, text='...', command=get_file)
-	getBadFileButton.grid(column=2, row=3, sticky=W)
-	getBadFileButton.columnconfigure(2, weight=1)
-	getBadFileButton.rowconfigure(3, weight=1)
+	get_bad_file_button = ttk.Button(setting_box, text='...', command=get_file)
+	get_bad_file_button.grid(column=2, row=3, sticky=W)
+	get_bad_file_button.columnconfigure(2, weight=1)
+	get_bad_file_button.rowconfigure(3, weight=1)
 
-	ttk.Label(settingBox, text='Bad Link File').grid(column=3, row=3, sticky=W)
+	ttk.Label(setting_box, text='Bad Link File').grid(column=3, row=3, sticky=W)
 
 	# Button to select word file
-	getWordFileButton = ttk.Button(settingBox, text='...', command=get_file)
-	getWordFileButton.grid(column=2, row=4, sticky=W)
-	getWordFileButton.columnconfigure(2, weight=1)
-	getWordFileButton.rowconfigure(4, weight=1)
+	get_word_file_button = ttk.Button(setting_box, text='...', command=get_file)
+	get_word_file_button.grid(column=2, row=4, sticky=W)
+	get_word_file_button.columnconfigure(2, weight=1)
+	get_word_file_button.rowconfigure(4, weight=1)
 
-	ttk.Label(settingBox, text='Word File').grid(column=3, row=4, sticky=W)
+	ttk.Label(setting_box, text='Word File').grid(column=3, row=4, sticky=W)
 
 	# Field to set MaxNewErrors
-	maxNewErrorEntry = ttk.Entry(settingBox, width=4, textvariable=MaxNewErrors)
-	maxNewErrorEntry.grid(column=2, row=5, sticky=(E, W))
-	maxNewErrorEntry.columnconfigure(2, weight=1)
-	maxNewErrorEntry.rowconfigure(5, weight=1)
+	max_new_error_entry = ttk.Entry(setting_box, width=4, textvariable=max_new_errors)
+	max_new_error_entry.grid(column=2, row=5, sticky=(E, W))
+	max_new_error_entry.columnconfigure(2, weight=1)
+	max_new_error_entry.rowconfigure(5, weight=1)
 
-	ttk.Label(settingBox, text='Max New Errors').grid(column=3, row=5, sticky=W)
+	ttk.Label(setting_box, text='Max New Errors').grid(column=3, row=5, sticky=W)
 
 	# Field to set MaxHTTPErrors
-	maxHTTPErrorEntry = ttk.Entry(settingBox, width=4, textvariable=MaxHTTPErrors)
-	maxHTTPErrorEntry.grid(column=2, row=6, sticky=(E, W))
-	maxHTTPErrorEntry.columnconfigure(2, weight=1)
-	maxHTTPErrorEntry.rowconfigure(6, weight=1)
+	max_http_error_entry = ttk.Entry(setting_box, width=4, textvariable=max_http_errors)
+	max_http_error_entry.grid(column=2, row=6, sticky=(E, W))
+	max_http_error_entry.columnconfigure(2, weight=1)
+	max_http_error_entry.rowconfigure(6, weight=1)
 
-	ttk.Label(settingBox, text='Max HTTP Errors').grid(column=3, row=6, sticky=W)
+	ttk.Label(setting_box, text='Max HTTP Errors').grid(column=3, row=6, sticky=W)
 
 	# Field to set MaxKnownErrors
-	maxKnownErrorsEntry = ttk.Entry(settingBox, width=4, textvariable=MaxKnownErrors)
-	maxKnownErrorsEntry.grid(column=2, row=7, sticky=(E, W))
-	maxKnownErrorsEntry.columnconfigure(2, weight=1)
-	maxKnownErrorsEntry.rowconfigure(7, weight=1)
+	max_known_errors_entry = ttk.Entry(setting_box, width=4, textvariable=max_known_errors)
+	max_known_errors_entry.grid(column=2, row=7, sticky=(E, W))
+	max_known_errors_entry.columnconfigure(2, weight=1)
+	max_known_errors_entry.rowconfigure(7, weight=1)
 
-	ttk.Label(settingBox, text='Max Known Errors').grid(column=3, row=7, sticky=W)
+	ttk.Label(setting_box, text='Max Known Errors').grid(column=3, row=7, sticky=W)
 
 	# Field to set MaxNewMIMEs
-	maxNewMIMEsEntry = ttk.Entry(settingBox, width=4, textvariable=MaxNewMIMEs)
-	maxNewMIMEsEntry.grid(column=2, row=8, sticky=(E, W))
-	maxNewMIMEsEntry.columnconfigure(2, weight=1)
-	maxNewMIMEsEntry.rowconfigure(8, weight=1)
+	max_new_mimes_entry = ttk.Entry(setting_box, width=4, textvariable=max_new_mimes)
+	max_new_mimes_entry.grid(column=2, row=8, sticky=(E, W))
+	max_new_mimes_entry.columnconfigure(2, weight=1)
+	max_new_mimes_entry.rowconfigure(8, weight=1)
 
-	ttk.Label(settingBox, text='Max New MIMEs').grid(column=3, row=8, sticky=W)
+	ttk.Label(setting_box, text='Max New MIMEs').grid(column=3, row=8, sticky=W)
 
 
 def start_crawler():
