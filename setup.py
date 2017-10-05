bc0eb6dd88c7e881be1cabe2ab9c3193f989b5cb
rivermont <william.11bennett@gmail.com>
2017-10-05 14:32:55 -0400

    Attempting to create a PyPi package.

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name="spidy",
+    packages=["spidy"],
+    version="1.4.0",
+    description="The simple command-line web crawler.",
+    author="Will Bennett",
+    author_email="william.11bennett@gmail.com",
+    url="https://github.com/rivermont/spidy",
+    download_url="https://github.com/rivermont/spidy/archive/1.4.tar.gz",
+    keywords=["spider", "web crawler", "scraping"],
+    classifiers=[],
+)
