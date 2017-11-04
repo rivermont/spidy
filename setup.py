d970e777689fc9123ed82c52d4840c9b67b91642
rivermont <william.11bennett@gmail.com>
2017-11-03 23:08:58 -0400

    spidy 1.6.4

--- setup.py
+++ setup.py
@@ -20,6 +20,9 @@ setup(
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Operating System :: OS Independent',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: MacOS',
         'Environment :: Console',
         'Natural Language :: English',
         'Programming Language :: Python',
@@ -28,7 +31,6 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
         'Topic :: Internet',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Indexing/Search'
