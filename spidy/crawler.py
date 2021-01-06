b42dcca769e9bca570ed6ddac4e900ef2463287f
Anton Avramov <lukav@lukav.com>
2021-01-06 10:42:01 -0500

    Fix generating robots.txt link

--- spidy/crawler.py
+++ spidy/crawler.py
@@ -214,7 +214,7 @@ class RobotsIndex(object):
 
     def _remember(self, url):
         urlparsed = urllib.parse.urlparse(url)
-        robots_url = url.replace(urlparsed.path, '/robots.txt')
+        robots_url = urlparsed.scheme + '://' + urlparsed.netloc + '/robots.txt'
         write_log('ROBOTS',
                   'Reading robots.txt file at: {0}'.format(robots_url),
                   package='reppy')
